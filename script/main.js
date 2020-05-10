3f9ffa2e7af590ed7dcc550d65652415362c8cd4
SansaSt <sc.comsa@mail.ru>
2020-05-10 13:45:01 +0500

    lesson27

--- script/main.js
+++ script/main.js
@@ -345,47 +345,59 @@ tabs();
     const statusMessage = document.createElement('div');
     statusMessage.style.cssText = 'fort-size: 2rem';
 
-    const postData = (body) => {
-      return new Promise ((resolve, reject) => {
-        const request = new XMLHttpRequest();
-        request.addEventListener('readystatechange', () => {
-          if (request.readyState !== 4) {
-            return;
-          }
-          if (request.status === 200) {
-           resolve();
-          } else {
-           reject(request.status);
-          }
-        });
-        request.open('POST', './server.php');
-        request.setRequestHeader('Content-Type', 'application/json');
-        request.send(JSON.stringify(body));
-      });
-
+    const validateTel = (tel) => {
+      let str = tel[0].value.replace('+', '').length;
+      if (str < 8) {
+        alert('Минимум 8 цифр');
+        return false;
+      } else {
+        return true;
+      }
 
     };
+
     forms.forEach(form => {
+
       form.addEventListener('input', (evt) => {
         let target = evt.target;
         if (target.name === 'user_phone') {
+          if (target.style) {
+            target.style.border = 'none';
+          }
           target.value = target.value.replace(/[^\+\d]/g, '');
         }
-
         if (target.name === 'user_name' || target.name === 'user_message') {
           target.value = target.value.replace(/[^а-я ]/gi, '');
         }
       });
 
+      const postData = (body) => {
+        console.log('body: ', body);
+        return fetch('./server.php', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(body),
+          mode: 'cors'
+        }); 
+      };
 
       form.addEventListener('submit', (event) => {
         event.preventDefault();
+        const formTel = [...event.target.elements].filter((item) => item.name === 'user_phone');
+        if (!validateTel(formTel)) {
+          return;
+        } 
+        
+        
         form.appendChild(statusMessage);
         statusMessage.style.cssText = `font-size: 2rem;
               color: #fff; `;
         const formData = new FormData(form);
         statusMessage.textContent = loadMessage;
 
+      
         let body = {};
         for (let val of formData.entries()) {
           body[val[0]] = val[1];
@@ -403,14 +415,21 @@ tabs();
               color: red; `;
             statusMessage.textContent = errorMessage;
         };
-
+        
 
         postData(body)
-          .then(outputData)
+          .then((response) => {
+            if (response.status !== 200) {
+                throw 'error !!! ';
+            }          
+            outputData();
+          })
           .catch(error);
 
       });
     });
+
+
   };
 
   sendForm();
